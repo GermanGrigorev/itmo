cfcbc5c7a9b5708558b0cee7b09e7be9a228bd3f
germangrigoriev <ggrigoriev.000spb@gmail.com>
2024-11-30 10:37:39 +0300

    chore: ui

--- shlapa/src/lib/entity/signified/ui/signified.svelte
+++ shlapa/src/lib/entity/signified/ui/signified.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-	import { Card } from 'flowbite-svelte';
+	import { Button, Card } from 'flowbite-svelte';
+	import { EyeSolid } from 'flowbite-svelte-icons';
 
 	let { text } = $props();
+
+	let isVisible = $state<boolean>(false);
 </script>
 
-<Card>
-	{text}
+<Card class="flex flex-row justify-between gap-10">
+	{#if isVisible}
+		{text}
+	{:else}
+		<div class="h-6 w-full animate-pulse rounded-lg bg-slate-300"></div>
+	{/if}
+	<Button on:click={() => (isVisible = !isVisible)} class="h-6 w-6"><EyeSolid /></Button>
 </Card>
