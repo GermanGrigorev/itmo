9a82ba2d09a200cb1ecc96968f654ad7dfed8147
germangrigoriev <ggrigoriev.000spb@gmail.com>
2024-12-07 10:43:30 +0300

    feat: docker

--- shlapa/svelte.config.js
+++ shlapa/svelte.config.js
@@ -1,21 +1,28 @@
-import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { sveltePreprocess } from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [sveltePreprocess(), optimizeImports()],
 
-    kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+	kit: {
+		// default is _app, but gh pages don't support special chars like _
+		appDir: 'application',
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			fallback: '/200.html'
+		}),
+		paths: {},
 
-    extensions: [".svelte", ".svx"]
+		prerender: {
+			handleHttpError: 'warn'
+			// error triggered when I changed mainfest /favicon to /head.svg
+			// https://kit.svelte.dev/docs/configuration#prerender
+		}
+	}
 };
 
 export default config;
